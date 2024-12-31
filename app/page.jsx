6899c0a166da1d1eda5db9ccba76dcf797b5f252
Rudolf Bok <rudolf.bok@outlook.cz>
2024-12-31 15:29:15 +0100

    height?2

--- app/page.jsx
+++ app/page.jsx
@@ -56,7 +56,7 @@ export default function Home() {
 
   return (
     <I18nextProvider i18n={i18n}>
-      <main className={`text-white font-inter ${backgroundClass} h-100vh`}>
+      <main className={`text-white font-inter ${backgroundClass} min-h-screen`}>
         <Header />
         <FavoritesSlider />
         <Hero />
--- components/DataCard.jsx
+++ components/DataCard.jsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function DataCard({ icon, headline, data }) {
   return (
-    <div className="flex flex-col backdrop-blur-3xl bg-black/5 rounded-3xl justify-between aspect-square text-white">
-      <div className="flex flex-row items-center backdrop-blur-sm rounded-bl-none rounded-br-none rounded-3xl p-3">
+    <div className="flex flex-col backdrop-blur-3xl bg-black/5 rounded-3xl h-full justify-between aspect-square text-white">
+      <div className="flex flex-row items-center rounded-bl-none border-b border-white rounded-3xl">
         <Image src={icon} alt={headline} height={25} width={25} />
-        <p className="text-xl ml-1 font-semibold">{headline}</p>
+        <p className="text-xl">{headline}</p>
       </div>
       <p className="text-2xl p-4">{data}</p>
     </div>
